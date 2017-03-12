9f2248321e1543a99d8f666752eaa71c04136ca7
lplb <lplb@outlook,com>
2017-03-11 20:16:08 -0500

    af

--- Assets/Scripts/Monde.cs
+++ Assets/Scripts/Monde.cs
@@ -7,6 +7,7 @@ public class Monde : MonoBehaviour {
     public int nbLoups = 1;
 	public float distanceAlignement = 10;
 	public float distanceSep = 10;
+    public GameObject cameraBerger;
 
     List<GameObject> moutons = new List<GameObject>();
     List<GameObject> loups = new List<GameObject>();
@@ -15,11 +16,13 @@ public class Monde : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        //Application.targetFrameRate = 4;
+        //QualitySettings.vSyncCount = 0;
         for (int i = 0; i < nbMoutons; i++) {
             moutons.Add((GameObject)Instantiate(Resources.Load("Mouton")));
-            moutons[i].transform.Translate(new Vector3(3*i,0,5*i));
-            moutons[i].GetComponent<Mover>().maxSpeed = 1;
-            moutons[i].GetComponent<Mover>().maxForce = 1;
+            moutons[i].transform.Translate(new Vector3(Random.Range(-25,25),0, Random.Range(-25, 25)));
+            moutons[i].GetComponent<Mover>().maxSpeed = 0.25f;
+            moutons[i].GetComponent<Mover>().maxForce = 0.5f;
             moutons[i].GetComponent<Mover>().targetRadius = 5;
         }
         for (int i = 0; i < nbLoups; i++) {
@@ -36,14 +39,18 @@ public class Monde : MonoBehaviour {
         updateMoutons();
         updateLoups();
         updateBerger();
+        updateCamera();
     }
 
     void updateMoutons(){
-       // alignement(moutons, 0.05f);
-        separation(moutons, 1);
+        alignement(moutons, 0.05f);
+        separation(moutons, 0.5f);
         foreach (GameObject mouton in moutons) {
-            mouton.GetComponent<Mover>().calculPhys();
-            mouton.GetComponent<Mover>().applyForce(mouton.GetComponent<Mover>().arrive(mousePos));
+            Mover mover = mouton.GetComponent<Mover>();
+            mover.calculPhys();
+            if (!mousePos.Equals(new Vector3(0,0,0)))
+                mover.applyForce(mover.arrive(mousePos));
+            mouton.transform.rotation = Quaternion.LookRotation(mover.vel);
             foreach(GameObject loup in loups) {
                 //mouton.GetComponent<Mover>().applyForce(0.5f * mouton.GetComponent<Mover>().flee(loup.transform.position));
             }
@@ -61,9 +68,14 @@ public class Monde : MonoBehaviour {
         berger.GetComponent<Mover>().calculPhys();
     }
 
+    void updateCamera() {
+        cameraBerger.GetComponent<Mover>().arrive(moutons[1].transform.position);
+        cameraBerger.GetComponent<Mover>().calculPhys();
+    }
+
     void click() {
         RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.current.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit) && hit.point != new Vector3(0,0,0)) {
             mousePos = hit.point;
         }
